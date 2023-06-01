5bef2a162ecb348bb2ca10dc3bf524ee344834f1
Eric Nguyen <enguyen807@gmail.com>
2023-06-01 16:57:51 +0300

    Add  flash message

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -68,6 +68,7 @@
 
                     // Register user
                     if ($this->userModel->register($data)) {
+                        flash('register_success', 'You are registered and can log in');
                         redirect('users/login');
                     } else {
                         die('Something went wrong');
