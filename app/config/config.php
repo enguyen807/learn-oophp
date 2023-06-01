8ef15fc57c742e2bdaa546868b42f690fe336a19
Eric Nguyen <enguyen807@gmail.com>
2023-06-01 16:00:40 +0300

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php 
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '@vw7d&VgdL%Xzp3sxFaL6PxCDH6bL&HkqLfWbn^h');
+    define('DB_NAME', 'shareposts');
+
+    // App Root
+    define('APPROOT', dirname(__DIR__, 1));
+    // URL Root
+    define('URLROOT', 'http://localhost/shareposts');
+    // Site Name
+    define('SITENAME', 'SharePosts');
+    // App Version
+    define('APPVERSION', '1.0.0');
---
+++ app/libraries/Controller.php
@@ -0,0 +1,33 @@
+<?php 
+    /**
+     * Base Controller
+     * loads the models and views
+     */
+    
+     class Controller {
+        // Load model
+        public function model($model) 
+        {
+            // Require modal file
+            require_once '../app/models/' . $model . '.php';
+
+            // Instantiate model
+            return new $model();
+        }
+
+        // Load view
+        /**
+         *  $view = file to be loaded for example posts/add
+         *  $data = dynamic values passed to the view
+         */
+        public function view($view, $data = []) {
+            // Check for view file
+            if (file_exists('../app/views/' . $view . '.php')) {
+                require_once '../app/views/' . $view . '.php';
+            } else {
+                // View does not exist
+                die('View does not exist');
+            }
+        }
+
+     }
