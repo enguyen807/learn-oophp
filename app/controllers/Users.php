05ee8b57602e363fabc471fb03e721e4bfead877
Eric Nguyen <enguyen807@gmail.com>
2023-06-05 17:12:25 +0300

    Add user session and logout

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -139,7 +139,7 @@
                     $loggedInUser = $this->userModel->login($data['email'], $data['password']);
                     if ($loggedInUser) {
                         // Create session variable
-                        die('SUCCESS');
+                        $this->createUserSession($loggedInUser);
                     } else {
                         $data['password_err'] = 'Password incorrect';
                         $this->view('users/login', $data);
@@ -161,6 +161,32 @@
                 // Load view
                 $this->view('users/login', $data);
 
+            }
+        }
+
+        public function createUserSession($user)
+        {
+            $_SESSION['user_id'] = $user->id;
+            $_SESSION['user_email'] = $user->email;
+            $_SESSION['user_name'] = $user->name;
+            redirect('pages/index');
+        }
+
+        public function logout()
+        {
+            unset($_SESSION['user_id']);
+            unset($_SESSION['user_email']);
+            unset($_SESSION['user_name']);
+            session_destroy();
+            redirect('users/login');
+        }
+
+        public function isLoggedIn()
+        {
+            if (isset($_SESSION['user_id'])) {
+                return true;
+            } else {
+                return false;
             }
         }
     }
