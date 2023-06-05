b67479fc36d4ce931ff8565bab5235227d99dd67
Eric Nguyen <enguyen807@gmail.com>
2023-06-05 22:35:51 +0300

    Add post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -21,11 +21,48 @@
 
         public function add() 
         {
-            $data = [
-                'title' => '',
-                'body' => ''
-            ];
+            if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                // SANITIZE POST
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
-            $this->view('posts/add', $data);
+                $data = [
+                    'title' => trim($_POST['title']),
+                    'body' => trim($_POST['body']),
+                    'user_id' => $_SESSION['user_id'],
+                    'title_err' => '',
+                    'body_err' => ''
+                ];
+
+                // Validate title
+                if (empty($data['title'])) {
+                    $data['title_err'] = 'Please enter title';
+                }
+
+                // Validate bodoy
+                if (empty($data['body'])) {
+                    $data['body_err'] = 'Please enter body text';
+                }
+
+                //Make sure no errors
+                if (empty($data['title_err']) && empty($data['body_err'])) {
+                    // Validated
+                    if ($this->postModel->addPost($data)) {
+                        flash('post_message', 'Post Added');
+                        redirect('posts');
+                    } else {
+                        die('Somethind went wrong');
+                    }
+                } else {
+                    $this->view('posts/add', $data);
+                }
+            } else {
+                $data = [
+                    'title' => '',
+                    'body' => ''
+                ];
+
+                $this->view('posts/add', $data);
+            }
+
         }
     }
