fe19ebbf857e107ad0cfde01e98b41391438f5b4
Eric Nguyen <enguyen807@gmail.com>
2023-06-05 22:08:19 +0300

    Add post model

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -5,10 +5,16 @@
             if (!isLoggedIn()) {
                 redirect('users/login');
             }
+
+            $this->postModel = $this->model('Post');
         }
 
         public function index () {
-            $data = [];
+            $posts = $this->postModel->getPosts();  
+
+            $data = [
+                'posts' => $posts
+            ];
 
             $this->view('posts/index', $data);
         }
